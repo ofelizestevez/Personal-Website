1d37bdcea378e5b81de83f80e85219252607c4df
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2022-02-08 19:39:08 -0500

    Various Changes
    Added a parser to parse though CSS to make the section cards. Resume Sections have been added. Resume Cards have been added.

---
+++ v3/main.js
@@ -0,0 +1,73 @@
+Papa.parse('./cards.csv', {
+    header: false,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+      data = results.data;
+      example(data)
+    }
+  });
+
+var targetDiv = document.getElementById("resume")
+
+function resume_container_mover(page){
+  pixel = page * -100
+  console.log( "translate($pixel %)".replace("$pixel",pixel))
+  document.getElementById("resume").style.transform = "translate($pixel%)".replace("$pixel",pixel)
+}
+
+function example(uArray){
+  for (let i = 1; i < uArray.length; i++) {
+    var targetID = uArray[i][0]
+    var title = uArray[i][1]
+    var link = uArray[i][2]
+    var subtitle_1 = uArray[i][3]
+    var subtitle_2 = uArray[i][4]
+    var paragraphs = uArray[i].slice(5)  
+      
+    card = document.createElement('div')
+    card.classList.add("resume-card")
+    
+    if(link == null){
+      // paragraph with card title classname
+      let title_element = document.createElement('p')
+      title_element.innerHTML = title
+      title_element.classList.add("card-title")
+      card.appendChild(title_element)
+    }
+    else{
+      // anchor with card title classname
+      let title_element = document.createElement('a')
+      title_element.href = link
+      title_element.innerHTML = title
+      title_element.classList.add("card-title")
+      card.appendChild(title_element)
+    }
+
+    subtitle_1_element = document.createElement("p")
+    subtitle_1_element.classList.add('card-subtitle')
+    subtitle_1_element.innerHTML = subtitle_1
+    subtitle_2_element = document.createElement("p")
+    subtitle_2_element.classList.add('card-subtitle')
+    subtitle_2_element.innerHTML = subtitle_2
+
+    card.appendChild(subtitle_1_element)
+    card.appendChild(subtitle_2_element)
+    card.appendChild(document.createElement('br'))
+    
+    for(let j = 0; j <paragraphs.length; j++){
+      if (paragraphs[j] != null){
+        let paragraph_element = document.createElement('p')
+        paragraph_element.innerHTML = paragraphs[j]
+        paragraph_element.classList.add('card-text')
+        card.appendChild(paragraph_element)
+      }
+
+    }
+
+    
+    document.getElementById(targetID).getElementsByClassName("card-container")[0].appendChild(card)
+
+        
+      }
+}
